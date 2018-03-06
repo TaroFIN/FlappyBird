5fc1f8549a449c129b4eb8c6178d52938fa39598
TaroFIN <32610351+TaroFIN@users.noreply.github.com>
2018-03-06 19:52:46 +0800

    Add files via upload

---
+++ Bird.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+#include "DEFINITIONS.hpp"
+#include "Game.hpp"
+#include <vector>
+
+namespace TaroFIN {
+	class Bird {
+	public:
+		Bird(GameDataRef data);
+
+		void Draw();
+
+		void Animate(float dt);
+
+	private:
+		GameDataRef _data;
+
+		sf::Sprite _birdSprite;
+		std::vector<sf::Texture> _animationFrames;
+
+		unsigned int _animationIterator;
+
+		sf::Clock _clock;
+
+	};
+}
---
+++ Pipe.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "Game.hpp"
+#include <vector>
+#include "DEFINITIONS.hpp"
+
+namespace TaroFIN {
+	class Pipe {
+	public:
+		Pipe(GameDataRef data);
+
+		void SpawnBottomPipe();
+		void SpawnTopPipe();
+		void SpawnInvisiblePipe();
+		void MovePipes(float dt);
+		void DrawPipes();
+		void RandomisePipeOffset();
+
+	private:
+		GameDataRef _data;
+		std::vector<sf::Sprite> pipeSprites;
+
+		int _landHeight;
+		int _pipeSpawnYoffset;
+	};
+}
